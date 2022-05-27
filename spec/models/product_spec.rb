359955f6f65a8497cc49710cb2a5e7171ae74ea7
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-27 15:03:13 -0700

    add a product error and callback

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+
+  it { should validate_presence_of :name }
+
+  it { should validate_length_of(:name).is_at_most(20)}
+
+  it("titleizes the name of a product") do
+    product = Product.create({name: "pizza crust", cost: "2", country_of_origin: "italy"})
+    expect(product.name).to(eq("Pizza Crust"))
+  end
 end
