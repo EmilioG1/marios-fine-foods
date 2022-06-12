77c178761e40a3554a8acb32fb53a3c36b9939c8
Emilio Gonzales <emiliog1184@gmail.com>
2022-06-12 12:08:17 -0700

    re-add content body validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates_numericality_of :rating, greater_than_or_equal_to: 1
   validates_numericality_of :rating, less_than_or_equal_to: 5
-  validates :content_body, presence: true, length: { minimum: 50 }
+  validates :content_body, presence: true, length: { minimum: 50, maximum: 250 }
   validates :product_id, presence: true
 
   before_save(:titleize_author)
